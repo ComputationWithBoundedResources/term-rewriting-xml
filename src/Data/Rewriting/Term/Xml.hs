4e8ae35482ec9d41406f923d90cfbf2089959431
Jonas Schöpf <jonas@schoepf.me>
2020-06-21 17:23:42 +0200

    error msg typos

--- src/Data/Rewriting/Term/Xml.hs
+++ src/Data/Rewriting/Term/Xml.hs
@@ -35,7 +35,7 @@ xmlToFunction xmlFunapp = T.Fun name args
     xmlArgs = map getArgTerm $ findChildren "arg" xmlFunapp
     args = map xmlToTerm' xmlArgs
     unText (Text t) = t
-    unText _        = error "Data.Rewriting.Term.Xml.xmlToFunction: somehting is wrong."
+    unText _        = error "Data.Rewriting.Term.Xml.xmlToFunction: something is wrong."
     getArgTerm = head.eChildren
 
 xmlToVariable :: UNode String -> T.Term String String
@@ -43,4 +43,4 @@ xmlToVariable xmlVar = T.Var name
   where
     name = concatMap unText $ eChildren xmlVar
     unText (Text t) = t
-    unText _        = error "Data.Rewriting.Term.Xml.xmlToVariabl: somehting is wrong."
+    unText _        = error "Data.Rewriting.Term.Xml.xmlToVariabl: something is wrong."
